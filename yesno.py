7cbaddf134b198fc6494f7431abd605ac194e209
Olaf Sarnow <olsarnow@gmail.com>
2019-04-04 17:06:41 +0200

    function

--- yesno.py
+++ yesno.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 
-yesChoice = ['yes', 'y']
-noChoice = ['no', 'n']
+def yes_or_no():
+  yesChoice = ['yes', 'y']
+  noChoice = ['no', 'n']
 
-input = raw_input("Would you like to play again? (y/N) ").lower()
+  input = raw_input("question? (y/N) ").lower()
 
-if input in yesChoice:
+  if input in yesChoice:
     print "ja"
-elif input in noChoice:
+  elif input in noChoice:
     print "nein"
     exit (0)
-else: 
+  else: 
     print "Enter Yes or No\n"
     exit (1)
+
+yes_or_no()
